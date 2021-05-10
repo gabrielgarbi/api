36e45382722dbd39451e8b8f53b0d76b90b72c85
gabrielgarbi <gabigarbijob@gmail.com>
2021-05-10 12:19:53 -0300

    Quiero ver si está realmente subiendose

--- src/main/java/edu/unsj/fcefn/lcc/optimizacion/api/ApiOptimizacionMogaApplication.java
+++ src/main/java/edu/unsj/fcefn/lcc/optimizacion/api/ApiOptimizacionMogaApplication.java
@@ -9,5 +9,5 @@ public class ApiOptimizacionMogaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiOptimizacionMogaApplication.class, args);
 	}
-
+ //quiero ver si se esta subiendo realmente
 }
